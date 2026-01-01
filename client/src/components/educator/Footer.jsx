f80dc020afccbae742b4d19f731c6c5c21c122d2
Abdomorad <moradabdo127@gmail.com>
2026-01-01 18:42:39 +0200

    last 11

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="flex items-center justify-between md:flex-row flex-col-reverse text-left w-full px-8 border-t">
       <div className="flex items-center gap-4 ">
-        <img src={assets.logo} alt="logo" srcset="" className='hidden md:block w-20' />
+        <img src={assets.brain} alt="logo" srcset="" className='hidden md:block w-20' />
         <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">© 2025 LMS. All rights reserved.</p>
       </div>
--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     }
     return (
         <div className={'flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b py-4 border-gray-500  ' + (isCourseListPage ? ' bg-white' : ' bg-cyan-100/70')}>
-            <img onClick={() => { navigate('/') }} src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
+            <img onClick={() => { navigate('/') }} src={assets.brain} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
             <div className='hidden md:flex items-center gap-5 text-gray-500'>
                 <div className='flex items-center gap-5'>
                     {user && <><button onClick={becomeEducator}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button> |
