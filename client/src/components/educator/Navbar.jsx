3118c87b46257d4d978a5046fc6be856a16cc636
Abdomorad <moradabdo127@gmail.com>
2026-01-01 19:08:58 +0200

    last 12

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className='w-full flex items-center justify-between md:px-8 border-b border-gray-500 px-4 py-3'>
       <Link to="/">
-      <img src={assets.brain} alt="logo" srcset="" className='w-28 lg:w-32'/>
+      <img src={assets.brain} alt="logo" srcset="" className='w-48'/>
       </Link>
       <div className='flex items-center gap-5 text-gray-500 relative'>
         <p className='text-gray-700 hidden md:block'>Hello, {user?.fullName || 'Developers'}</p>
